f3fc201c0fccf970d0097754d3e5a568764f797e
wang323@khu.ac.kr <wang323@khu.ac.kr>
2021-10-14 22:25:58 +0900

    eight to nine

--- ggd_8.py
+++ ggd_8.py
@@ -1,3 +1,4 @@
-for dan in range(2, 9):  # 2 ~ 8, change needed
+for dan in range(2, 10):  # 2 ~ 8, change needed
+    #2~9, change complete
     for i in range(1, 10):
         print(f'{dan} x {i} = {dan * i}')
